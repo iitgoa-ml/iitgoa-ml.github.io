13d95f00298c42d83a0fd1854701c3c56f0b2ca7
Shivam Kumar <shivam.kumar.16001@iitgoa.ac.in>
2018-12-27 21:17:23 +0530

    Added one more member

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 // Profile Links
-var shivam_kumar_profile_link = "https://www.linkedin.com/in/shivamkumar7/"
-var shivam_pandey_profile_link = "https://www.linkedin.com/in/shivam-pandey-744841136/"
+var shivam_kumar_profile_link = "https://www.linkedin.com/in/shivamkumar7/";
+var shivam_pandey_profile_link =
+  "https://www.linkedin.com/in/shivam-pandey-744841136/";
+var abhilasha_gupta_profile_link =
+  "https://www.linkedin.com/in/abhilasha-gupta-30748011a";
