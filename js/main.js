615fd71e327d38323036d174f31ed83bca7772a3
Shivam Kumar <shivam.kumar.16001@iitgoa.ac.in>
2019-01-17 07:06:35 +0530

    Added Abhishek Varghese in people page

--- js/main.js
+++ js/main.js
@@ -4,3 +4,4 @@ var shivam_pandey_profile_link =
   "https://www.linkedin.com/in/shivam-pandey-744841136/";
 var abhilasha_gupta_profile_link =
   "https://www.linkedin.com/in/abhilasha-gupta-30748011a";
+var abhishek_varghese_profile_link = "https://www.linkedin.com/in/abhishek-varghese-583483154/"
