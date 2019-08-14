560f39bf38890df6be624ba42ce6ac877d931a96
Clint P. George <1380705+clintpgeorge@users.noreply.github.com>
2019-08-14 14:55:05 +0530

    ML Seminar 1

--- js/meetings.js
+++ js/meetings.js
@@ -10,35 +10,42 @@ class Meeting {
 
 var meeting_list = new Array();
 meeting_list.push(new Meeting(
-    date_and_time = '3-4 PM, January 17, 2019',
+    date_and_time = 'Jan 17, 2019, 3-4 pm',
     venue = 'LH-1',
     topic = 'ML Circle Inception',
     abstract_link = 'meetings/abstracts/ML Circle Inception Abstract.pdf',
     slides_link = 'meetings/slides/ML Circle Inception.pdf'));
 
 meeting_list.push(new Meeting(
-    date_and_time = '3:10 - 4:10 PM, January 31, 2019',
+    date_and_time = 'Jan 31, 2019, 3:10-4:10 pm',
     venue = 'LH-2',
     topic = 'Linear regression using Gradient Descent (by Abhishek Varghese)',
     abstract_link = 'meetings/abstracts/ML Circle _ Meeting - 1.pdf',
     slides_link = 'https://prezi.com/view/fDCGpAX3gdSSuiulon8C/'));
     
 meeting_list.push(new Meeting(
-    date_and_time = '3 PM onwards, February 2, 2019',
+    date_and_time = 'Feb 02, 2019, 3 pm',
     venue = 'LH-2',
     topic = 'Linear regression using Gradient Descent (by Abhishek Varghese)',
     abstract_link = 'meetings/abstracts/ML Circle _ Meeting - 1.pdf',
     slides_link = 'https://prezi.com/view/fDCGpAX3gdSSuiulon8C/'));
 
 meeting_list.push(new Meeting(
-    date_and_time = '5 PM onwards, February 14, 2019',
+    date_and_time = 'Feb 14, 2019, 5 pm',
     venue = 'LH-2',
     topic = 'Convergence analysis of gradient descent (by Shivam Pandey)',
     abstract_link = 'meetings/abstracts/ML Circle _ Meeting - 2.pdf'));
 
 meeting_list.push(new Meeting(
-    date_and_time = '5:30 - 6:30 PM, April 2, 2019',
+    date_and_time = 'Apr 02, 2019, 5:30-6:30 pm',
     venue = 'CL-3',
     topic = 'Introduction to Convex Optimization (by Abhilasha Gupta and Mahesh Barve)',
     abstract_link = 'meetings/abstracts/ML Circle _ Meeting - 3.pdf',
     slides_link = 'meetings/slides/Convex_Optimization__ML_circle.pdf'));
+
+    meeting_list.push(new Meeting(
+        date_and_time = 'Aug 14, 2019, 3-4 pm',
+        venue = 'CL-2',
+        topic = 'Topic Modeling: General Introduction to Learning by Sreejith A. V.',
+        abstract_link = '',
+        slides_link = ''));
